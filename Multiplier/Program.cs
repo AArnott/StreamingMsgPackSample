326209761c82660c4eb87c735c83b6effed2a393
Andrew Arnott <andrewarnott@gmail.com>
2020-03-27 07:33:14 -0600

    Add project files.

---
+++ Multiplier/Program.cs
@@ -0,0 +1,19 @@
+﻿using MessagePack;
+using System;
+using System.Threading.Tasks;
+
+namespace Multiplier
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var stdout = Console.OpenStandardOutput();
+            for (int i = 0; i < 15; i++)
+            {
+                await MessagePackSerializer.SerializeAsync(stdout, new string('a', i));
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
---
+++ StreamingMsgPackSample/Program.cs
@@ -0,0 +1,36 @@
+﻿using MessagePack;
+using System;
+using System.Buffers;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StreamingMsgPackSample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (s, e) =>
+            {
+                Console.WriteLine("Canceling...");
+                cts.Cancel();
+                e.Cancel = true;
+            };
+
+            var psi = new ProcessStartInfo(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"..\..\..\..\Multiplier\bin\Debug\netcoreapp3.1\Multiplier.exe"));
+            psi.RedirectStandardOutput = true;
+            using var child = Process.Start(psi);
+            using var msgpackReader = new MessagePackStreamReader(child.StandardOutput.BaseStream);
+            ReadOnlySequence<byte>? msgpackSequence;
+            while ((msgpackSequence = await msgpackReader.ReadAsync(cts.Token)).HasValue)
+            {
+                string json = MessagePackSerializer.ConvertToJson(msgpackSequence.Value);
+                Console.WriteLine(json);
+            }
+        }
+    }
+}
